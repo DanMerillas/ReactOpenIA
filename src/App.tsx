e9c82ab38f18168bd8a89655f78614671b9b7586
Daniel Merillas <dmerillasg@minsait.com>
2023-02-11 16:06:37 +0100

    problema netlify

--- src/App.tsx
+++ src/App.tsx
@@ -6,63 +6,63 @@ import LoadingSpinner from './LoadingSpinner';
 
 function App() {
 
-  const [response, setResponse] = useState("");
-  const [responseImagen, setResponseImagen] = useState("");
-  const [inputText, setInputText] = useState("");
-  const [inputTextImage, setinputTextImage] = useState("");
-  const [loading, setLoading] = useState(false);
+  // const [response, setResponse] = useState("");
+  // const [responseImagen, setResponseImagen] = useState("");
+  // const [inputText, setInputText] = useState("");
+  // const [inputTextImage, setinputTextImage] = useState("");
+  // const [loading, setLoading] = useState(false);
 
 
-  async function fetchData() {
-    setLoading(true)
-    const res = await fetch("https://api.openai.com/v1/completions", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        "Authorization": process.env.REACT_APP_API_KEY ? process.env.REACT_APP_API_KEY : ''
-      },
-      body: JSON.stringify({
-        prompt: inputText,
-        model: "text-davinci-003",
-        max_tokens: 4000,
-        temperature: 0
-      })
-    });
-    const data = await res.json();
-    setLoading(false)
-    setResponse(data.choices[0].text);
+  // async function fetchData() {
+  //   setLoading(true)
+  //   const res = await fetch("https://api.openai.com/v1/completions", {
+  //     method: "POST",
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //       "Authorization": process.env.REACT_APP_API_KEY ? process.env.REACT_APP_API_KEY : ''
+  //     },
+  //     body: JSON.stringify({
+  //       prompt: inputText,
+  //       model: "text-davinci-003",
+  //       max_tokens: 4000,
+  //       temperature: 0
+  //     })
+  //   });
+  //   const data = await res.json();
+  //   setLoading(false)
+  //   setResponse(data.choices[0].text);
 
-  }
+  // }
 
-  async function fetchDataImagen() {
-    setLoading(true)
-    const res = await fetch("https://api.openai.com/v1/images/generations", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        "Authorization": process.env.REACT_APP_API_KEY ? process.env.REACT_APP_API_KEY : ''
-      },
-      body: JSON.stringify({
-        prompt: inputTextImage,
-        n: 1,
-        size: "512x512"
-      })
-    });
-    const data = await res.json();
-    setLoading(false)
-    setResponseImagen(data.data[0].url);
-  }
+  // async function fetchDataImagen() {
+  //   setLoading(true)
+  //   const res = await fetch("https://api.openai.com/v1/images/generations", {
+  //     method: "POST",
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //       "Authorization": process.env.REACT_APP_API_KEY ? process.env.REACT_APP_API_KEY : ''
+  //     },
+  //     body: JSON.stringify({
+  //       prompt: inputTextImage,
+  //       n: 1,
+  //       size: "512x512"
+  //     })
+  //   });
+  //   const data = await res.json();
+  //   setLoading(false)
+  //   setResponseImagen(data.data[0].url);
+  // }
 
-  const handleKeyDown = (event:any)=>{
-    if (event.key === 'Enter') {
-      fetchData();
-    }
-  } 
-  const handleKeyDownImagen = (event:any)=>{
-    if (event.key === 'Enter') {
-      fetchData();
-    }
-  } 
+  // const handleKeyDown = (event:any)=>{
+  //   if (event.key === 'Enter') {
+  //     fetchData();
+  //   }
+  // } 
+  // const handleKeyDownImagen = (event:any)=>{
+  //   if (event.key === 'Enter') {
+  //     fetchData();
+  //   }
+  // } 
 
 
 
