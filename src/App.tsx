e24e6e39d1500ef436500c53c92d8560c1f7d7ba
Daniel Merillas <dmerillasg@minsait.com>
2023-02-11 15:11:58 +0100

    prueba con open IA a falta de maquetar

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,83 @@
-import  { useEffect, useState } from 'react';
+import { useState } from 'react';
 
 import './App.css';
-import OpenAIAPI from "react-openai-api";
+import LoadingSpinner from './LoadingSpinner';
+
 
 function App() {
 
-  const [response, setResponse] = useState('')
-  
-const callOpenIA  = ()=>{
-   OpenAIAPI({
-    apiKey:process.env.REACT_APP_API_KEY ? process.env.REACT_APP_API_KEY : '',
-    payload:{
-      prompt: "Mario: Hi, how are you?",
-      maxTokens: 25,
-      temperature: 0.5,
-      n: 1,
-    },
-    responseHandler:responseHandler
-  })
-}
- const responseHandler = (response: any) => {
-  setResponse(response.choices[0].text)
-}
-   
-  //callOpenIA()
+  const [response, setResponse] = useState("");
+  const [responseImagen, setResponseImagen] = useState("");
+  const [inputText, setInputText] = useState("");
+  const [inputTextImage, setinputTextImage] = useState("");
+  const [loading, setLoading] = useState(false);
 
- 
+
+  async function fetchData() {
+    setLoading(true)
+    const res = await fetch("https://api.openai.com/v1/completions", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": process.env.REACT_APP_API_KEY ? process.env.REACT_APP_API_KEY : ''
+      },
+      body: JSON.stringify({
+        prompt: inputText,
+        model: "text-davinci-003",
+        max_tokens: 4000,
+        temperature: 0
+      })
+    });
+    const data = await res.json();
+    setLoading(false)
+    setResponse(data.choices[0].text);
+
+  }
+
+  async function fetchDataImagen() {
+    setLoading(true)
+    const res = await fetch("https://api.openai.com/v1/images/generations", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": process.env.REACT_APP_API_KEY ? process.env.REACT_APP_API_KEY : ''
+      },
+      body: JSON.stringify({
+        prompt: inputTextImage,
+        n: 1,
+        size: "512x512"
+      })
+    });
+    const data = await res.json();
+    setLoading(false)
+    setResponseImagen(data.data[0].url);
+  }
+
+  const handleKeyDown = (event:any)=>{
+    if (event.key === 'Enter') {
+      fetchData();
+    }
+  } 
+  const handleKeyDownImagen = (event:any)=>{
+    if (event.key === 'Enter') {
+      fetchData();
+    }
+  } 
+
+
 
   return (
-    <>
-      <p>{response}</p>
-    </>
+    <div className='App'>
+      {loading ? <LoadingSpinner /> : <>
+      <h1>Preguntas:</h1>
+      <input type={'text'} onChange={(e) => setInputText(e.target.value)} onKeyDown={handleKeyDown} /><input type={'button'} value="Enviar" onClick={fetchData} />
+      {response !== "" && <><h1>Repuesta:</h1><p>{response}</p></>}
+
+      <h1>Genera imagenes:</h1>
+      <input type={'text'} onChange={(e) => setinputTextImage(e.target.value)} onKeyDown={(handleKeyDownImagen)}/><input type={'button'} value="Enviar" onClick={fetchDataImagen} />
+      {responseImagen !== "" && <><h1> Imagen:</h1><img alt='' src={responseImagen} /></>}
+      </>}
+    </div>
   );
 }
 
